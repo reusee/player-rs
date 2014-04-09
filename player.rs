9c1ed6a30f651fb3c6e34fb82b4395c8f3cbd4af
reus <reusee@gmail.com>
2014-04-09 23:58:24 +0800

    segfault

--- player.rs
+++ player.rs
@@ -1,10 +1,10 @@
 #![feature(globs)]
 
 extern crate libc;
+extern crate sdl2;
+extern crate avformat;
 
-mod ffmpeg;
 mod video;
-mod sdl;
 
 #[link(name="avformat")]
 #[link(name="avcodec")]
@@ -15,13 +15,43 @@ extern {}
 fn main() {
   // init
   unsafe {
-    ffmpeg::av_register_all();
+    avformat::av_register_all();
   }
+
+  // sdl
+  let (width, height) = (1024, 768);
+  sdl2::init([sdl2::InitVideo, sdl2::InitAudio, sdl2::InitTimer]);
+  let window = match sdl2::video::Window::new("player",
+    sdl2::video::PosCentered, sdl2::video::PosCentered,
+    width, height, [sdl2::video::Maximized, sdl2::video::Resizable]) {
+    Ok(w) => w,
+    Err(e) => fail!(format!("create window {}", e)),
+  };
+  let renderer = match sdl2::render::Renderer::from_window(window, sdl2::render::DriverAuto,
+    [sdl2::render::Accelerated]) {
+    Ok(r) => r,
+    Err(e) => fail!(format!("create renderer {}", e)),
+  };
+  let texture = match renderer.create_texture(sdl2::pixels::YV12, sdl2::render::AccessStreaming,
+    width, height) {
+    Ok(t) => t,
+    Err(e) => fail!(format!("create texture {}", e)),
+  };
 
   // video
   let video = match video::Video::new(~"m.mp4") {
     Ok(v) => v,
     Err(e) => fail!(e),
   };
-  drop(video);
+  if video.video_streams.is_empty() || video.audio_streams.is_empty() {
+    fail!("no video or no audio");
+  }
+
+  // decode
+  let (timed_frame_in, timed_frame_out) = channel();
+  let decoder = video.decode(*video.video_streams.get(0), *video.audio_streams.get(0),
+    width, height,
+    timed_frame_in);
+
+  // render
 }
--- video.rs
+++ video.rs
@@ -1,52 +1,56 @@
-use ffmpeg;
-use std::ptr::{array_each_with_len, mut_null};
+extern crate time;
+extern crate avformat;
+extern crate avcodec;
+extern crate avutil;
+extern crate swscale;
+
+use std::ptr::{array_each_with_len, mut_null, null};
+use std::cast::transmute;
+use std::mem::size_of;
 
 pub struct Video {
-  format_context: *ffmpeg::AVFormatContext,
-  streams: Vec<*ffmpeg::AVStream>,
-  video_streams: Vec<*ffmpeg::AVStream>,
-  audio_streams: Vec<*ffmpeg::AVStream>,
+  pub format_context: *avformat::AVFormatContext,
+  pub streams: Vec<*avformat::AVStream>,
+  pub video_streams: Vec<*avformat::AVStream>,
+  pub audio_streams: Vec<*avformat::AVStream>,
 }
 
 impl Video {
   pub fn new(filename: ~str) -> Result<Video, &'static str> { unsafe {
     // format context
-    let mut format_context: *mut ffmpeg::AVFormatContext = mut_null();
-    if ffmpeg::avformat_open_input(&mut format_context, filename.to_c_str().unwrap(), mut_null(), mut_null()) != 0 {
+    let mut format_context: *mut avformat::AVFormatContext = mut_null();
+    if avformat::avformat_open_input(&mut format_context, filename.to_c_str().unwrap(), mut_null(), mut_null()) != 0 {
       return Err("open input");
     }
-    if ffmpeg::avformat_find_stream_info(format_context, mut_null()) < 0 {
+    if avformat::avformat_find_stream_info(format_context, mut_null()) < 0 {
       return Err("find stream info error");
     }
-    ffmpeg::av_dump_format(format_context, 0, filename.to_c_str().unwrap(), 0);
+    avformat::av_dump_format(format_context, 0, filename.to_c_str().unwrap(), 0);
 
     // streams
     let mut streams = Vec::new();
     let mut video_streams = Vec::new();
     let mut audio_streams = Vec::new();
-    array_each_with_len((*format_context).streams as **ffmpeg::AVStream,
+    array_each_with_len((*format_context).streams as **avformat::AVStream,
       (*format_context).nb_streams as uint, {|stream| {
       streams.push(stream);
       let codec = (*stream).codec;
       match (*codec).codec_type {
-        ffmpeg::AVMEDIA_TYPE_VIDEO => video_streams.push(stream),
-        ffmpeg::AVMEDIA_TYPE_AUDIO => audio_streams.push(stream),
+        avutil::AVMEDIA_TYPE_VIDEO => video_streams.push(stream),
+        avutil::AVMEDIA_TYPE_AUDIO => audio_streams.push(stream),
         _ => (), //TODO
       };
     }});
-    if video_streams.is_empty() || audio_streams.is_empty() {
-      return Err("no audio or no video");
-    }
 
     // codecs
     for stream in streams.iter() {
       let codecCtx = (**stream).codec;
-      let codec = ffmpeg::avcodec_find_decoder((*codecCtx).codec_id);
+      let codec = avcodec::avcodec_find_decoder((*codecCtx).codec_id);
       if codec.is_null() {
         return Err("no decoder");
       }
-      let mut options: *mut ffmpeg::AVDictionary = mut_null();
-      if ffmpeg::avcodec_open2(codecCtx, &*codec, &mut options) < 0 {
+      let mut options: *mut avutil::AVDictionary = mut_null();
+      if avcodec::avcodec_open2(codecCtx, &*codec, &mut options) < 0 {
         return Err("open codec error");
       }
     }
@@ -59,13 +63,101 @@ impl Video {
     })
   }}
 
+  pub fn decode(&self, video_stream: *avformat::AVStream, audio_stream: *avformat::AVStream,
+    width: int, height: int, timed_frame_in: Sender<*avcodec::AVFrame>) -> Decoder { unsafe {
+    let vcodec_ctx = (*video_stream).codec;
+    let acodec_ctx = (*audio_stream).codec;
+
+    // frame pool
+    let (pool_in, pool_out) = channel();
+    let pool_size = 16;
+    let buffer_size = avcodec::avpicture_get_size(avutil::PIX_FMT_YUV420P,
+      width as i32, height as i32);
+    let mut frames = Vec::new();
+    let mut buffers = Vec::new();
+    for _ in range(0, pool_size) {
+      let frame = avcodec::avcodec_alloc_frame();
+      frames.push(frame);
+      let buffer = avutil::av_malloc(buffer_size as u64);
+      buffers.push(buffer as *mut u8);
+      avcodec::avpicture_fill(frame as *mut avcodec::AVPicture, buffer as *u8,
+        avutil::PIX_FMT_YUV420P, width as i32, height as i32);
+      pool_in.send(frame);
+    }
+
+    let decoder = Decoder{
+      pool_in: pool_in,
+      frames: frames,
+      buffers: buffers,
+      start_time: time::now(),
+    };
+
+    // decode
+    let (frame_in, frame_out) = channel();
+    let format_context = self.format_context as *mut avformat::AVFormatContext ;
+    spawn(proc() {
+      // scale context
+      let scale_context = swscale::sws_getCachedContext(mut_null(),
+        (*vcodec_ctx).width, (*vcodec_ctx).height, (*vcodec_ctx).pix_fmt,
+        width as i32, height as i32, avutil::PIX_FMT_YUV420P,
+        0x200, mut_null(), mut_null(), null());
+      if scale_context.is_null() {
+        fail!("get scale context");
+      }
+
+      let mut packet: *mut avcodec::AVPacket = unsafe {
+        transmute(avutil::av_malloc(size_of::<avcodec::AVPacket>() as u64))
+      };
+      avcodec::av_init_packet(packet);
+      let mut frame_finished: i32 = -1;
+      let plane_size: int;
+      let vframe = avcodec::avcodec_alloc_frame();
+      let aframe = avcodec::avcodec_alloc_frame();
+      let mut pts: f64 = 0f64;
+
+      while avformat::av_read_frame(format_context, packet) >= 0 {
+        avcodec::av_free_packet(packet);
+        if (*packet).stream_index == (*video_stream).index { // video
+          if avcodec::avcodec_decode_video2(vcodec_ctx, vframe, &mut frame_finished, &*packet) < 0 {
+            continue
+          }
+          if (*packet).dts != avutil::AV_NOPTS_VALUE { // get pts
+            pts = (*packet).dts as f64;
+          } else { // more pts info
+            fail!("FIXME: no pts info") //TODO
+          }
+          pts *= avutil::av_q2d((*video_stream).time_base) * 1000 as f64;
+          if frame_finished > 0 { // got frame
+            let buf_frame = pool_out.recv();
+            swscale::sws_scale(scale_context, &((*vframe).data[0]), &((*vframe).linesize[0]),
+              0, (*vcodec_ctx).height,
+              &mut((*buf_frame).data[0] as *u8), &((*buf_frame).linesize[0]));
+            (*buf_frame).pts = pts as i64;
+            frame_in.send(buf_frame);
+          }
+        } else {
+        //TODO audio and others
+        }
+      }
+    });
+
+    return decoder
+  }}
+
 }
 
 impl Drop for Video {
   fn drop(&mut self) { unsafe {
-    ffmpeg::avformat_close_input(&mut(self.format_context as *mut ffmpeg::AVFormatContext));
+    avformat::avformat_close_input(&mut(self.format_context as *mut avformat::AVFormatContext));
     //for stream in self.streams.iter() {
     //  ffmpeg::avcodec_close((**stream).codec);
     //}
   }}
+}
+
+struct Decoder {
+  pool_in: Sender<*mut avcodec::AVFrame>,
+  frames: Vec<*mut avcodec::AVFrame>,
+  buffers: Vec<*mut u8>,
+  start_time: time::Tm,
 }
